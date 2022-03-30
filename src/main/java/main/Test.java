9380d3765881eb13b103022b887b8a6331cdd3e6
Mathis <mathistryla@gmail.com>
2022-03-30 10:37:21 +0200

    insert new string sout test class

--- src/main/java/main/Test.java
+++ src/main/java/main/Test.java
@@ -11,7 +11,7 @@ package main;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test + new");
     }
     
 }
