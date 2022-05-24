67834e656b706dd547ba6ede055d9c81c7c7cc8c
Mathis <mathistryla@gmail.com>
2022-05-24 15:35:02 +0200

    propal°2 arbre

--- src/main/java/com/rein/solution/Arbre.java
+++ src/main/java/com/rein/solution/Arbre.java
@@ -6,12 +6,16 @@ import com.rein.instance.Noeud;
 import com.rein.io.InstanceReader;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Map;
 
 public class Arbre {
 
     private Noeud noeudRacine;
     private ArrayList<Arbre> listeFils;
+    static int cpt = 0;
+    static final int PROFONDEUR_MAX = 10;
 
     public Arbre(Noeud noeudRacine) {
         this.noeudRacine = noeudRacine;
@@ -40,11 +44,23 @@ public class Arbre {
         this.noeudRacine = noeudRacine;
     }
 
-    public static void creerArbre(Arbre arbre){
-        Arbre a = new Arbre(arbre);
-        a.remplirListeFils();
-        for(Arbre fils : a.getListeFils())
-            creerArbre(fils);
+    public static void creerArbre(Arbre arbre, HashSet<Integer> setId){
+        cpt += 1;
+        if(cpt < PROFONDEUR_MAX){
+            Arbre a = new Arbre(arbre);
+            a.remplirListeFils();
+            for(Arbre fils : a.getListeFils())
+                System.out.println("\tracine=" + a.noeudRacine.getId() + " -> " + fils.noeudRacine.getId());
+            System.out.println("\n");
+            if(!a.noeudRacine.isPresent(setId)){
+                for(Arbre fils : a.getListeFils()){
+                    setId.add(fils.noeudRacine.getId());
+                    creerArbre(fils, setId);
+                }
+            }
+            /*for(Arbre fils : a.getListeFils())
+                creerArbre(fils);*/
+        }
     }
 
     public int taille(Arbre a){
@@ -78,9 +94,12 @@ public class Arbre {
         try{
             InstanceReader reader = new InstanceReader("instancesInitiales/KEP_p9_n1_k3_l3.txt");
             Instance i = reader.readInstance();
-            //System.out.println(i.getEchanges());
             Arbre a = new Arbre(i.getTabNoeud()[0]);
-            creerArbre(a);
+            HashSet<Integer> setId = new HashSet<>();
+            creerArbre(a, setId);
+            System.out.println(a);
+            for(Arbre fils : a.getListeFils())
+                System.out.println("\t" + fils);
         } catch(Exception e){
             System.err.println(e);
         }
