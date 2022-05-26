d59d32c655a4f8c6f434d61a753c0a533ade1e84
Shahpnico <nicolashahpazian@gmail.com>
2022-05-27 00:38:02 +0200

    Commit intermédiaire de construction d'arbres

--- src/main/java/com/rein/solution/Arbre.java
+++ src/main/java/com/rein/solution/Arbre.java
@@ -1,30 +1,38 @@
 package com.rein.solution;
 
-import com.rein.instance.Echange;
 import com.rein.instance.Instance;
 import com.rein.instance.Noeud;
 import com.rein.io.InstanceReader;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.*;
 
 public class Arbre {
 
+    private int id;
     private Noeud noeudRacine;
     private ArrayList<Arbre> listeFils;
-    static int cpt = 0;
-    static final int PROFONDEUR_MAX = 10;
+    private int niveauProfondeur;
+    static final int PROFONDEUR_MAX = 5;
+    static ArrayList<HashSet<Integer>> listeChainesPossibles = new ArrayList<HashSet<Integer>>();
+    static ArrayList<HashSet<Integer>> listeCyclesPossibles = new ArrayList<HashSet<Integer>>();
+
+    // --------------------------------------------------
+    // --------------------------------------------------
 
     public Arbre(Noeud noeudRacine) {
+        this.id = noeudRacine.getId();
         this.noeudRacine = noeudRacine;
         this.listeFils = new ArrayList<>();
+        this.niveauProfondeur = 0;
     }
 
     public Arbre(Noeud noeudRacine, ArrayList<Arbre> listeFils) {
         this(noeudRacine);
         this.listeFils = listeFils;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public Arbre(Arbre arbre) {
@@ -44,24 +52,52 @@ public class Arbre {
         this.noeudRacine = noeudRacine;
     }
 
-    public static void creerArbre(Arbre arbre, HashSet<Integer> setId){
-        cpt += 1;
-        if(cpt < PROFONDEUR_MAX){
-            Arbre a = new Arbre(arbre);
-            a.remplirListeFils();
-            for(Arbre fils : a.getListeFils())
-                System.out.println("\tracine=" + a.noeudRacine.getId() + " -> " + fils.noeudRacine.getId());
-            System.out.println("\n");
-            if(!a.noeudRacine.isPresent(setId)){
-                for(Arbre fils : a.getListeFils()){
-                    setId.add(fils.noeudRacine.getId());
-                    creerArbre(fils, setId);
+
+
+    // -- Methode chargée d'entamer la création d'un arbre (1er level)
+    // Fait ensuite appel de façon récursive à
+    public static void recurrArbre(Arbre arbre, LinkedHashSet<Integer> listeId, int profondeur){
+        //this.niveauProfondeur += 1;
+        profondeur++;
+        LinkedHashSet<Integer> listeIdBis = new LinkedHashSet<>(listeId);
+        System.out.println("On est sur : " + arbre.getId());
+        if (listeIdBis.add(arbre.getId())) {
+            if(profondeur < PROFONDEUR_MAX){
+                arbre.remplirListeFils();       //Récupération de ses fils
+                for(Arbre fils : arbre.getListeFils()) {
+                    //System.out.println("\tracine=" + a.noeudRacine.getId() + " -> " + fils.noeudRacine.getId());
+                    //System.out.println("\n");"";
+                    System.out.println("On descend sur : " + fils.getId());
+                    recurrArbre(fils, listeIdBis, profondeur);
+                    System.out.println("On remonte...");
                 }
+            }else {
+                System.out.println("Detection d'une chaine !!! ---> " + listeIdBis);
+                listeChainesPossibles.add(listeId);
             }
-            /*for(Arbre fils : a.getListeFils())
-                creerArbre(fils);*/
+        }else {
+            Iterator it = listeIdBis.iterator();
+            System.out.println("Detection d'un cycle !!! ---> " + listeIdBis);
+            int idCourant = 0;
+            while (idCourant != arbre.getId()) {
+                //System.out.println("aaaa");
+                idCourant = (int) it.next();
+            }
+                listeIdBis.remove(idCourant);
+            System.out.println("Detection d'un cycle !!! ---> " + listeIdBis);
+            listeCyclesPossibles.add(listeId);
         }
     }
+
+    // -- Méthode récursive de génération de l'arbre
+    // appelée par creerArbre()
+    public void genArbreRecursif(Arbre a) {
+
+    }
+
+
+
+
 
     public int taille(Arbre a){
         if(a == null) return 0;
@@ -75,9 +111,13 @@ public class Arbre {
         for(Map.Entry echange : this.noeudRacine.getListeEchanges().entrySet()){
             Noeud noeudFils = (Noeud) echange.getKey();
             Arbre a = new Arbre(noeudFils);
+            a.niveauProfondeur = this.niveauProfondeur + 1;
             this.listeFils.add(a);
         }
     }
+
+    // --------------------------------------------------
+    // --------------------------------------------------
 
     @Override
     public String toString() {
@@ -90,16 +130,24 @@ public class Arbre {
                 ", listeFils=[" + listeFils + "]}";
     }
 
+
     public static void main(String[] args) {
         try{
-            InstanceReader reader = new InstanceReader("instancesInitiales/KEP_p9_n1_k3_l3.txt");
+            InstanceReader reader = new InstanceReader("instancesInitiales/KEP_p100_n11_k5_l17.txt");
             Instance i = reader.readInstance();
             Arbre a = new Arbre(i.getTabNoeud()[0]);
-            HashSet<Integer> setId = new HashSet<>();
-            creerArbre(a, setId);
+            LinkedHashSet<Integer> listeId = new LinkedHashSet<Integer>();
+            recurrArbre(a, listeId, 0);
             System.out.println(a);
-            for(Arbre fils : a.getListeFils())
-                System.out.println("\t" + fils);
+            /*for(Arbre fils : a.getListeFils())
+                System.out.println("\t" + fils);*/
+            System.out.println(i.getEchanges());
+            System.out.println("id : " + a.getId());
+            System.out.println("On est bons");
+            System.out.println("listeCyclesPossibles : ");
+            System.out.println(listeCyclesPossibles);
+            System.out.println("listeChainesPossibles : ");
+            System.out.println(listeChainesPossibles);
         } catch(Exception e){
             System.err.println(e);
         }
